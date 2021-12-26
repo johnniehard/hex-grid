e66760181c35222a6b91b5868d77fc0f19392fe7
Johnnie Hård <johnnie@synvinkel.org>
2021-12-26 11:00:52 +0100

    Drawing hexes.

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import Hexagon from "./Hexagon.svelte";
+</script>
+
+<main>
+	<Hexagon />
+	<Hexagon width={50} height={50} />
+</main>
+
+<style>
+	:global(body) {
+		background: rgb(11, 4, 37);
+		height: 100%;
+
+		display: grid;
+		place-items: center;
+	}
+</style>
