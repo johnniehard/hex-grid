03a66433be12a3b217e4b3e7b5c0762384811fc0
Johnnie Hård <johnnie@synvinkel.org>
2021-12-28 21:15:20 +0100

    fix: PUBLIC_URL for gh-pages.

---
+++ src/Circle.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    export let x: number;
+    export let y: number;
+    export let r: number;
+
+    export let active: boolean = false;
+</script>
+
+<circle cx={x} cy={y} {r} on:click on:mouseenter class={active ? "active" : ""} />
+
+<style>
+    circle {
+        fill: rgba(0, 0, 0, 0);
+        /* transition: fill 0.5s ease; */
+    }
+
+    circle:hover {
+        fill: rgb(253, 34, 154);
+        transition: none;
+    }
+
+    .active {
+        fill: rgb(253, 34, 154);
+        transition: none;
+        /* transition: fill .15s ease; */
+    }
+</style>
--- src/Hexagon.svelte
+++ src/Hexagon.svelte
@@ -16,17 +16,14 @@
 <style>
     path {
         fill: rgba(0, 0, 0, 0);
-        transition: fill 0.5s ease;
     }
-
+    
     path:hover {
         fill: rgb(253, 34, 154);
         transition: none;
     }
-
+    
     .active {
         fill: rgb(253, 34, 154);
-        transition: none;
-        /* transition: fill .15s ease; */
     }
 </style>
