d4cd6934188d6843a726344b16758c525b82dfdb
Johnnie Hård <johnnie@synvinkel.org>
2021-12-31 09:26:14 +0100

    WIP: Separate displaying grid from play/pause/clear logic.

--- src/App.svelte
+++ src/App.svelte
@@ -1,52 +1,46 @@
 <script lang="ts">
-	import Svg from "./Svg.svelte";
-	import Hexagon from "./Hexagon.svelte";
-	import { gridSetup, OurGrid } from "./hexagon";
-	import clone from "just-clone";
 	import queryString from "query-string";
+	import { onMount } from "svelte";
+	import Life from "./Life.svelte";
 
-	// TODO: Layout isolated component
-	// Make a component Life that takes grid parameters like radius and seed,
-	// and exposes functions like clear, togglePlay etc.
-	// This way we can more easily do things like show thumbnails of seeds,
+	// TODO: Seed gallery
+	// Thumbnails of seeds. Click to set main grid seed.
 	// play multiple grids at once, or show a none-interactive grid that cycles between
 	// playing different seeds (at different speeds).
 
 	const r = 10;
-	const birthRule = [2];
-	const survivalRule = [2, 4];
 
-	let grid = gridSetup(r);
 	let play = false;
 	let interval: ReturnType<typeof setInterval> | null = null;
 	let generations = 0;
 
-	const width = grid.pointWidth();
-	const height = grid.pointHeight();
+	let life: Life;
 
 	let seed = [];
 
-	const qs = queryString.parse(location.search, {
-		parseNumbers: true,
-		arrayFormat: "bracket-separator",
-		arrayFormatSeparator: ",",
+	onMount(() => {
+		const qs = queryString.parse(location.search, {
+			parseNumbers: true,
+			arrayFormat: "bracket-separator",
+			arrayFormatSeparator: ",",
+		});
+
+		if (qs.seed && Array.isArray(qs.seed)) {
+			seed = (qs.seed as any[]).filter(
+				(x) => !isNaN(parseInt(x))
+			) as number[];
+			play = seed.length > 0;
+		}
+
+		if (seed.length > 0) {
+			clear(false);
+			life.setSeed(seed);
+		} else {
+			clear();
+		}
 	});
 
-	if (qs.seed && Array.isArray(qs.seed)) {
-		seed = (qs.seed as any[]).filter(
-			(x) => !isNaN(parseInt(x))
-		) as number[];
-		play = seed.length > 0;
-	}
-
-	if (seed.length > 0) {
-		clear(false);
-		setSeed(seed);
-	} else {
-		clear();
-	}
-
-	$: showGrid = generations === 0
+	$: showGrid = generations === 0;
 
 	$: if (play) {
 		interval = setInterval(step, 200);
@@ -70,34 +64,8 @@
 		);
 	}
 
-	function life(inputGrid: OurGrid, birth: number[], survival: number[]) {
-		const prevGrid = clone(inputGrid);
-		const newGrid = inputGrid.map((hex, i) => {
-			const prevHex = prevGrid[i];
-			const neighbors = prevHex.neighbors.map((n) => prevGrid[n]);
-			const aliveNeighbors = neighbors.filter((n) => n.alive);
-			const alive = prevHex.alive;
-
-			if (!alive) {
-				if (birth.includes(aliveNeighbors.length)) {
-					hex.alive = true;
-					return hex;
-				}
-			}
-
-			if (survival.includes(aliveNeighbors.length)) {
-				return hex;
-			} else {
-				hex.alive = false;
-				return hex;
-			}
-		});
-
-		return newGrid;
-	}
-
 	function step() {
-		grid = life(grid, birthRule, survivalRule);
+		life.step();
 		generations += 1;
 	}
 
@@ -110,53 +78,25 @@
 	}
 
 	function clear(clearSeed = true) {
-		generations = 0;
-		grid = gridSetup(r);
-		seed = clearSeed ? [] : seed;
-	}
-
-	// TODO: Make this a pure function (grid) => grid, and move it to hexagon.ts
-	function setSeed(seed: number[]) {
-		grid = grid.map((hex, i) => {
-			hex.alive = seed.includes(i);
-			return hex;
-		});
+			generations = 0;
+			life.clear()
+			seed = clearSeed ? [] : seed;
 	}
 
 	function reset() {
 		clear(false);
-		setSeed(seed);
+		life.setSeed(seed);
 		pause();
 	}
 </script>
 
 <main>
-	<Svg {width} {height}>
-		{#each grid.map( (hex) => ({ hex, point: hex.toPoint() }) ) as { hex, point }, i}
-			<Hexagon
-				active={hex.alive}
-				showOutline={showGrid}
-				on:click={() => {
-					hex.alive = !hex.alive;
-					if (!play) {
-						if (hex.alive) {
-							seed = [...seed, i];
-						} else {
-							seed = seed.filter((s) => s !== i);
-						}
-					}
-				}}
-				{r}
-				x={point.x + width / 2}
-				y={point.y + height / 2}
-			/>
-		{/each}
-	</Svg>
+	<Life bind:this={life} {r} {showGrid} />
 	<button
-	on:click={() => {
-		clear();
-		pause();
-	}}>clear</button
+		on:click={() => {
+			clear();
+			pause();
+		}}>clear</button
 	>
 	<button on:click={reset}>reset</button>
 	<button on:click={togglePlay}>{play ? "pause" : "play"}</button>
---
+++ src/Life.svelte
@@ -0,0 +1,81 @@
+<script lang="ts">
+    import { gridSetup, OurGrid } from "./hexagon";
+    import clone from "just-clone";
+
+    import Svg from "./Svg.svelte";
+    import Hexagon from "./Hexagon.svelte";
+
+    export let r = 10;
+    export let birthRule = [2];
+    export let survivalRule = [2, 4];
+    export let showGrid = false;
+
+    let grid = gridSetup(r);
+
+    const width = grid.pointWidth();
+    const height = grid.pointHeight();
+
+    export function step() {
+        grid = life(grid, birthRule, survivalRule);
+    }
+
+    export function clear() {
+        grid = gridSetup(r);
+    }
+
+    // TODO: Make this a pure function (grid) => grid, and move it to hexagon.ts
+    export function setSeed(seed: number[]) {
+        grid = grid.map((hex, i) => {
+            hex.alive = seed.includes(i);
+            return hex;
+        });
+    }
+
+    function life(inputGrid: OurGrid, birth: number[], survival: number[]) {
+        const prevGrid = clone(inputGrid);
+        const newGrid = inputGrid.map((hex, i) => {
+            const prevHex = prevGrid[i];
+            const neighbors = prevHex.neighbors.map((n) => prevGrid[n]);
+            const aliveNeighbors = neighbors.filter((n) => n.alive);
+            const alive = prevHex.alive;
+
+            if (!alive) {
+                if (birth.includes(aliveNeighbors.length)) {
+                    hex.alive = true;
+                    return hex;
+                }
+            }
+
+            if (survival.includes(aliveNeighbors.length)) {
+                return hex;
+            } else {
+                hex.alive = false;
+                return hex;
+            }
+        });
+
+        return newGrid;
+    }
+</script>
+
+<Svg {width} {height}>
+    {#each grid.map( (hex) => ({ hex, point: hex.toPoint() }) ) as { hex, point }, i}
+        <Hexagon
+            active={hex.alive}
+            showOutline={showGrid}
+            on:click={() => {
+                hex.alive = !hex.alive;
+                // if (!play) {
+                //     if (hex.alive) {
+                //         seed = [...seed, i];
+                //     } else {
+                //         seed = seed.filter((s) => s !== i);
+                //     }
+                // }
+            }}
+            {r}
+            x={point.x + width / 2}
+            y={point.y + height / 2}
+        />
+    {/each}
+</Svg>
