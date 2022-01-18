a81d543bf41057b97867ddb4981359d550b3d60e
Johnnie Hård <johnnie@synvinkel.org>
2022-01-18 21:21:48 +0100

    Dynamically create HTML file that points to the latest hash.

--- rollup.config.js
+++ rollup.config.js
@@ -7,8 +7,44 @@ import sveltePreprocess from 'svelte-preprocess';
 import typescript from '@rollup/plugin-typescript';
 import css from 'rollup-plugin-css-only';
 import replace from '@rollup/plugin-replace';
+import html from "@rollup/plugin-html";
 
 const production = !process.env.ROLLUP_WATCH;
+const version = String(
+	require("child_process").execSync("git rev-parse --short HEAD")
+).trim();
+
+const htmlOptions = {
+	template: async ({ attributes, files, meta, publicPath, title }) => {
+		const script = (files.js || [])
+			.map(({ fileName }) => {
+				return `<script defer src='./${fileName}'></script>`;
+			})
+			.join("\n");
+
+		const css = (files.css || [])
+			.map(({ fileName }) => {
+				return `<link rel='stylesheet' href='./${fileName}'>`;
+			})
+			.join("\n");
+		return `<!DOCTYPE html>
+				<html lang="en">
+				<head>
+					<meta charset='utf-8'>
+					<meta name='viewport' content='width=device-width,initial-scale=1'>
+
+					<title>hex</title>
+
+					${css}
+					${script}
+				</head>
+
+				<body>
+				</body>
+				</html>
+				`;
+	},
+};
 
 function serve() {
 	let server;
@@ -37,7 +73,7 @@ export default {
 		sourcemap: true,
 		format: 'iife',
 		name: 'app',
-		file: 'public/build/bundle.js'
+		file: `public/build/bundle.${version}.js`
 	},
 	plugins: [
 		svelte({
@@ -49,7 +85,7 @@ export default {
 		}),
 		// we'll extract any component CSS out into
 		// a separate file - better for performance
-		css({ output: 'bundle.css' }),
+		css({ output: `bundle.${version}.css` }),
 
 		// If you have external dependencies installed from
 		// npm, you'll most likely need these plugins. In
@@ -81,6 +117,7 @@ export default {
 			'process.env.PUBLIC_URL': JSON.stringify(process.env.PUBLIC_URL),
 			preventAssignment: true,
 		}),
+		html(htmlOptions),
 	],
 	watch: {
 		clearScreen: false
