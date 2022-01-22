05935fe0dd89372d69dad9255bebfdaed09a110c
Johnnie Hård <johnnie@synvinkel.org>
2022-01-22 13:38:21 +0100

    Remove unused.

--- src/App.svelte
+++ src/App.svelte
@@ -127,11 +127,4 @@
 		display: grid;
 		place-items: center;
 	}
-
-	.gallery.item {
-		width: 100px;
-		border: 1px solid rgba(253, 34, 154, 0.4);
-		border-radius: 5px;
-		cursor: pointer;
-	}
 </style>
